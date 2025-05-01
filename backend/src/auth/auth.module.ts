140e85a189bce545f48ba5e3c4e7458098fd3687
pstamayo7 <pstamayo@pucesi.edu.ec>
2025-05-01 00:45:25 -0500

    se probo y corrigio un error en las rutas de archivos

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { UsersModule } from './users/users.module'; // Importamos el UsersModule aquí
+import { UsersModule } from '../users/users.module';  // Importamos el UsersModule aquí
 
 @Module({
   imports: [

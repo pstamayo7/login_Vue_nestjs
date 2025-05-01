233acee1bccbb993216ec4aab02b8a73d1aa6942
pstamayo7 <pstamayo@pucesi.edu.ec>
2025-04-30 23:49:54 -0500

    Se añadio el swagger a la configuracion del backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Login System API')
+    .setDescription('API para login con JWT')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
